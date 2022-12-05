8614b1dea24da2b6b7c54830c8e098d93ab1eed7
baboda212 <baboda212@naver.com>
2022-12-05 11:33:11 +0900

    입력내용 추가1

--- src/index.js
+++ src/index.js
@@ -1,35 +1,44 @@
 //index.js
-//추가(입력)버튼을 클릭시 실행될 함수(추가함수)
+//1. 추가(입력)버튼을 클릭시 실행될 함수(추가함수)
 function handleAdd() {
     //console.log('추가'); //확인용
-    //memo입력요소
+    //4. memo입력요소
     const memoEl = document.getElementById('memo');
     
-    //li 문서객체생성
+    //5. li 문서객체생성
     const li = document.createElement('li');
 
-    //p 생성
+    //6. p 생성
     const p = document.createElement('p');
     console.log(li, p); //확인용
 
-    //input(memo)입력값 읽기 : value값
+    //7. input(memo)입력값 읽기 : value값
     console.log(memoEl.value);
 
-    //p요소에 입력값을 추가
+    //8. p요소에 입력값을 추가
     p.textContent = memoEl.value;
     console.log(p);
     memoEl.value = ''; //입력완료 후 내용비움(초기화)
 
-    
+    //9. 삭제버튼 생성
+    const delBtn = document.createElement('button');
+    delBtn.textContent = '삭제';
+    console.log(delBtn);    
+
+    //10. li요소에 p와 삭제버튼 넣기
+    li.appendChild(p);
+    li.appendChild(delBtn);
+    console.log(li); //확인용
+
 
 };
 
 
 
-//입력버튼 - addBtn 가져오기
+//2. 입력버튼 - addBtn 가져오기
 const addBtn = document.getElementById('addBtn');
 
-//입력(추가)버튼 이벤트
+//3. 입력(추가)버튼 이벤트
 addBtn.addEventListener('click', () => {
     handleAdd(); //추가함수 호출
 
